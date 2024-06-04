3a7904809edd0a2539ef607ff0531cad7762f89a
Edgar Simo-Serra <ess@waseda.jp>
2024-06-04 20:40:43 +0900

    Ignore directories that start with _.

--- export2gltf.sh
+++ export2gltf.sh
@@ -4,7 +4,9 @@ if [ -z "$1" ]
 then
    for i in */*.blend
    do
-      $0 "$i"
+      if [[ "${i:0:1}" != "_" ]]; then
+         $0 "$i"
+      fi
    done
 else
    blender "$1" -b -P export2gltf.py -- "$1" || exit 1
