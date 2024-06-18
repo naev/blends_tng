5bdfb2ba18d243ac4814cb5d3392341f6d2e62db
Edgar Simo-Serra <ess@waseda.jp>
2024-06-18 17:12:55 +0900

    don't use gltf-transform optimize as it can eat nodes.

--- export2gltf.py
+++ export2gltf.py
@@ -36,25 +36,34 @@ bpy.ops.export_scene.gltf( filepath=blenderpath, export_format='GLTF_SEPARATE', 
         export_extras=True, export_apply=True, use_visible=True )
 
 # And optimize
-ret = subprocess.run(["gltf-transform", "optimize", "--compress", "false", "--texture-compress", "webp", "--simplify", "false", "--instance", "false", "--texture-size", "1024", blenderpath, gltfpath ])
-if ret.returncode != 0:
-    print("Problem optimizing mesh!")
-    sys.exit(-1)
-
-"""
-os.chdir(shipdir)
-with open(shipname + '.mtl') as mtlfile:
-    mtltext = mtlfile.read()
-
-for i in os.listdir():
-    name, ext = os.path.splitext(i)
-    if ext not in ('.obj', '.mtl', '.png'):
-        oldname, ext, i = i, '.png', f'{name}.png'
-        print(f'Converting {oldname} to {i}...')
-        subprocess.call(['convert', oldname, i])
-        os.unlink(oldname)
-        mtltext = mtltext.replace(oldname, i)
-
-with open(shipname + '.mtl', 'w') as mtlfile:
-    mtlfile.write(mtltext)
-"""
+commands = (
+    ["dedup",],
+    ["instance",],
+    ["palette",],
+    #["flatten",], # breaks special nodes
+    #["join",], # breaks special nodes
+    #["weld",], # breaks special nodes
+    #["simplify",], # breaks special nodes
+    ["resample",],
+    ["prune", "--keep-leaves", "true",],
+    ["sparse",],
+    ["resize", "--width", "1024", "--height", "1024",],
+    ["webp", "--lossless", "true", "--effort", "100",],
+    #["meshopt"], # not supported by Naev
+)
+#commands = (
+#    ["optimize", "--compress", "false", "--texture-compress", "webp", "--simplify", "false", "--instance", "false", "--texture-size", "1024"],
+#)
+def run_command( c, pathin, pathout ):
+    cmd = ["gltf-transform",] + c + [pathin, pathout]
+    ret = subprocess.run( cmd )
+    if ret.returncode != 0:
+        print("Problem optimizing mesh!")
+        sys.exit(-1)
+    return pathout
+pathin = blenderpath
+for c in commands[:-1]:
+    pathout = f"{blenderdir.name}/{blenderbase}/{blenderbase}_{c[0]}.gltf"
+    run_command( c, pathin, pathout )
+    pathin = pathout
+run_command( commands[-1], pathin, gltfpath )
